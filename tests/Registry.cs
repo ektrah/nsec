a0ddc9e3088993a12f116fce8fcb85b517908bd6
ektrah <klaus.hartke@gmail.com>
2017-01-01 18:36:02 +0100

    Update registry

--- tests/Registry.cs
+++ tests/Registry.cs
@@ -10,6 +10,7 @@ namespace NSec.Tests
 
         public static readonly TheoryData<Type> AeadAlgorithms = new TheoryData<Type>
         {
+            typeof(ChaCha20Poly1305),
         };
 
         public static readonly TheoryData<Type> AuthenticationAlgorithms = new TheoryData<Type>
@@ -50,6 +51,7 @@ namespace NSec.Tests
 
         public static readonly TheoryData<Type> SymmetricAlgorithms = new TheoryData<Type>
         {
+            typeof(ChaCha20Poly1305),
         };
 
         public static readonly TheoryData<Type> KeylessAlgorithms = new TheoryData<Type>
@@ -63,17 +65,31 @@ namespace NSec.Tests
         public static readonly TheoryData<Type, KeyBlobFormat> PublicKeyBlobFormats = new TheoryData<Type, KeyBlobFormat>
         {
             { typeof(X25519), KeyBlobFormat.RawPublicKey },
+            { typeof(X25519), KeyBlobFormat.NSecPublicKey },
+            { typeof(X25519), KeyBlobFormat.PkixPublicKey },
+            { typeof(X25519), KeyBlobFormat.PkixPublicKeyText },
             { typeof(Ed25519), KeyBlobFormat.RawPublicKey },
+            { typeof(Ed25519), KeyBlobFormat.NSecPublicKey },
+            { typeof(Ed25519), KeyBlobFormat.PkixPublicKey },
+            { typeof(Ed25519), KeyBlobFormat.PkixPublicKeyText },
         };
 
         public static readonly TheoryData<Type, KeyBlobFormat> PrivateKeyBlobFormats = new TheoryData<Type, KeyBlobFormat>
         {
             { typeof(X25519), KeyBlobFormat.RawPrivateKey },
+            { typeof(X25519), KeyBlobFormat.NSecPublicKey },
+            { typeof(X25519), KeyBlobFormat.PkixPrivateKey },
+            { typeof(X25519), KeyBlobFormat.PkixPrivateKeyText },
             { typeof(Ed25519), KeyBlobFormat.RawPrivateKey },
+            { typeof(Ed25519), KeyBlobFormat.NSecPrivateKey },
+            { typeof(Ed25519), KeyBlobFormat.PkixPrivateKey },
+            { typeof(Ed25519), KeyBlobFormat.PkixPrivateKeyText },
         };
 
         public static readonly TheoryData<Type, KeyBlobFormat> SymmetricKeyBlobFormats = new TheoryData<Type, KeyBlobFormat>
         {
+            { typeof(ChaCha20Poly1305), KeyBlobFormat.RawSymmetricKey },
+            { typeof(ChaCha20Poly1305), KeyBlobFormat.NSecSymmetricKey },
         };
 
         #endregion
