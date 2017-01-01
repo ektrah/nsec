076fe91611858d47cc351b512b9d3d58663b1248
ektrah <klaus.hartke@gmail.com>
2017-01-01 17:08:54 +0100

    Add tests for RNG

---
+++ tests/Core/RandomTests.cs
@@ -0,0 +1,64 @@
+using System;
+using NSec.Cryptography;
+using Xunit;
+
+namespace NSec.Tests.Core
+{
+    public static class RandomTests
+    {
+        #region GenerateBytes #1
+
+        [Fact]
+        public static void GenerateBytesWithNegativeCount()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>("count", () => SecureRandom.GenerateBytes(-1));
+        }
+
+        [Fact]
+        public static void GenerateBytesWithZeroCount()
+        {
+            var bytes = SecureRandom.GenerateBytes(0);
+
+            Assert.NotNull(bytes);
+            Assert.Equal(0, bytes.Length);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(15)]
+        [InlineData(63)]
+        public static void GenerateBytesWithCount(int count)
+        {
+            var bytes = SecureRandom.GenerateBytes(count);
+
+            Assert.NotNull(bytes);
+            Assert.Equal(count, bytes.Length);
+            Assert.NotEqual(new byte[count], bytes);
+        }
+
+        #endregion
+
+        #region GenerateBytes #2
+
+        [Fact]
+        public static void GenerateBytesWithEmptySpan()
+        {
+            SecureRandom.GenerateBytes(new byte[0]);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(15)]
+        [InlineData(63)]
+        public static void GenerateBytesWithSpan(int count)
+        {
+            var bytes = new byte[count];
+
+            SecureRandom.GenerateBytes(bytes);
+
+            Assert.NotEqual(new byte[count], bytes);
+        }
+
+        #endregion
+    }
+}
