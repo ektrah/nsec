7822dd5a9462d0d8c46b0fce8eecac8d74a9ec16
ektrah <klaus.hartke@gmail.com>
2017-01-01 16:49:57 +0100

    Add alloc/export/import methods to SecureMemoryHandle

--- src/Interop/Interop.SecureMemoryHandle.cs
+++ src/Interop/Interop.SecureMemoryHandle.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 internal static partial class Interop
@@ -7,11 +8,63 @@ internal static partial class Interop
     {
         internal sealed class SecureMemoryHandle : SafeHandle
         {
+            private int _length;
+
             private SecureMemoryHandle() : base(IntPtr.Zero, true)
             {
             }
 
             public override bool IsInvalid => (handle == IntPtr.Zero);
+
+            public int Length => _length;
+
+            public static SecureMemoryHandle Alloc(int length)
+            {
+                Debug.Assert(length >= 0);
+
+                SecureMemoryHandle handle = sodium_malloc((IntPtr)length);
+                handle._length = length;
+                return handle;
+            }
+
+            public unsafe Span<byte> DangerousGetSpan()
+            {
+                return new Span<byte>(handle.ToPointer(), _length);
+            }
+
+            public void Export(Span<byte> span)
+            {
+                bool addedRef = false;
+                try
+                {
+                    DangerousAddRef(ref addedRef);
+                    DangerousGetSpan().CopyTo(span);
+                }
+                finally
+                {
+                    if (addedRef)
+                    {
+                        DangerousRelease();
+                    }
+                }
+            }
+
+            public void Import(ReadOnlySpan<byte> span)
+            {
+                bool addedRef = false;
+                try
+                {
+                    DangerousAddRef(ref addedRef);
+                    span.CopyTo(DangerousGetSpan());
+                }
+                finally
+                {
+                    if (addedRef)
+                    {
+                        DangerousRelease();
+                    }
+                }
+            }
 
             public void MakeReadOnly()
             {
