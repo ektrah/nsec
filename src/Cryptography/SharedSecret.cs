090c829f252c5e096065e05f8948859ef5824be1
ektrah <klaus.hartke@gmail.com>
2017-01-01 16:51:49 +0100

    Add SharedSecret class

---
+++ src/Cryptography/SharedSecret.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Diagnostics;
+using static Interop.Libsodium;
+
+namespace NSec.Cryptography
+{
+    [DebuggerDisplay("Size = {Size}")]
+    public sealed class SharedSecret : IDisposable
+    {
+        private readonly SecureMemoryHandle _handle;
+
+        internal SharedSecret(
+            SecureMemoryHandle handle)
+        {
+            Debug.Assert(handle != null);
+
+            _handle = handle;
+            _handle.MakeReadOnly();
+        }
+
+        public int Size => _handle.Length;
+
+        internal SecureMemoryHandle Handle => _handle;
+
+        public static SharedSecret Import(
+            ReadOnlySpan<byte> sharedSecret)
+        {
+            if (sharedSecret.Length > 128)
+                throw new ArgumentException();
+            if (!Sodium.TryInitialize())
+                throw new InvalidOperationException();
+
+            SecureMemoryHandle handle = SecureMemoryHandle.Alloc(sharedSecret.Length);
+            handle.Import(sharedSecret);
+            return new SharedSecret(handle);
+        }
+
+        public void Dispose()
+        {
+            _handle.Dispose();
+        }
+    }
+}
