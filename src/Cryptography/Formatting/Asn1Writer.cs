70788e3654116ca5596ac5c453eb77534d5af9e1
ektrah <klaus.hartke@gmail.com>
2017-01-25 01:34:11 +0100

    Add Asn1Writer

---
+++ src/Cryptography/Formatting/Asn1Writer.cs
@@ -0,0 +1,165 @@
+//#define UNSAFE
+
+using System;
+using System.Diagnostics;
+#if UNSAFE
+using System.Runtime.CompilerServices;
+#endif
+
+namespace NSec.Cryptography.Formatting
+{
+#if UNSAFE
+    unsafe 
+#endif
+    internal struct Asn1Writer
+    {
+        private const int StackSize = 8;
+
+#if UNSAFE
+        private void* _bytes;
+#else
+        private Span<byte> _bytes;
+#endif
+        private int _depth;
+        private int _pos;
+        private int[] _stack;
+
+        public Asn1Writer(ref Span<byte> bytes)
+        {
+#if UNSAFE
+            _bytes = Unsafe.AsPointer(ref bytes);
+#else
+            _bytes = bytes;
+#endif
+            _depth = 0;
+            _pos = bytes.Length;
+            _stack = new int[StackSize];
+        }
+
+#if UNSAFE
+        public ReadOnlySpan<byte> Bytes => Unsafe.AsRef<Span<byte>>(_bytes).Slice(_pos);
+#else
+        public ReadOnlySpan<byte> Bytes => _bytes.Slice(_pos);
+#endif
+
+        public void BeginSequence()
+        {
+            Debug.Assert(_depth != 0);
+            _depth--;
+            WriteLength(_stack[_depth] - _pos);
+            WriteByte(0x30);
+        }
+
+        public void End()
+        {
+            Debug.Assert(_depth != StackSize);
+            _stack[_depth] = _pos;
+            _depth++;
+        }
+
+        public void Integer(uint value)
+        {
+            int end = _pos;
+            unchecked
+            {
+                WriteByte((byte)value);
+                if ((value & 0xFFFFFF80U) != 0)
+                    WriteByte((byte)(value >> 8));
+                if ((value & 0xFFFF8000U) != 0)
+                    WriteByte((byte)(value >> 16));
+                if ((value & 0xFF800000U) != 0)
+                    WriteByte((byte)(value >> 24));
+            }
+            WriteLength(end - _pos);
+            WriteByte(0x02);
+        }
+
+        public void Integer(ulong value)
+        {
+            int end = _pos;
+            unchecked
+            {
+                WriteByte((byte)value);
+                if ((value & 0xFFFFFFFFFFFFFF80U) != 0)
+                    WriteByte((byte)(value >> 8));
+                if ((value & 0xFFFFFFFFFFFF8000U) != 0)
+                    WriteByte((byte)(value >> 16));
+                if ((value & 0xFFFFFFFFFF800000U) != 0)
+                    WriteByte((byte)(value >> 24));
+                if ((value & 0xFFFFFFFF80000000U) != 0)
+                    WriteByte((byte)(value >> 32));
+                if ((value & 0xFFFFFF8000000000U) != 0)
+                    WriteByte((byte)(value >> 40));
+                if ((value & 0xFFFF800000000000U) != 0)
+                    WriteByte((byte)(value >> 48));
+                if ((value & 0xFF80000000000000U) != 0)
+                    WriteByte((byte)(value >> 56));
+            }
+            WriteLength(end - _pos);
+            WriteByte(0x02);
+        }
+
+        public void ObjectIdentifier(ReadOnlySpan<byte> oid)
+        {
+            WriteBytes(oid);
+            WriteLength(oid.Length);
+            WriteByte(0x06);
+        }
+
+        public void OctetString(ReadOnlySpan<byte> octets)
+        {
+            WriteBytes(octets);
+            WriteLength(octets.Length);
+            WriteByte(0x04);
+        }
+
+        private void WriteByte(byte value)
+        {
+            if (_pos == 0)
+                throw new ArgumentException();
+
+            _pos--;
+#if UNSAFE
+            Unsafe.AsRef<Span<byte>>(_bytes)[_pos] = value;
+#else
+            _bytes[_pos] = value;
+#endif
+        }
+
+        private void WriteBytes(ReadOnlySpan<byte> bytes)
+        {
+            if (bytes.Length > _pos)
+                throw new ArgumentException();
+
+            _pos -= bytes.Length;
+#if UNSAFE
+            bytes.CopyTo(Unsafe.AsRef<Span<byte>>(_bytes).Slice(_pos));
+#else
+            bytes.CopyTo(_bytes.Slice(_pos));
+#endif
+        }
+
+        private void WriteLength(int length)
+        {
+            if (length < 0x80)
+            {
+                WriteByte((byte)length);
+            }
+            else
+            {
+                int end = _pos;
+                unchecked
+                {
+                    WriteByte((byte)length);
+                    if ((length & 0xFFFFFF00U) != 0)
+                        WriteByte((byte)(length >> 8));
+                    if ((length & 0xFFFF0000U) != 0)
+                        WriteByte((byte)(length >> 16));
+                    if ((length & 0xFF000000U) != 0)
+                        WriteByte((byte)(length >> 24));
+                }
+                WriteByte((byte)(0x80 | (end - _pos)));
+            }
+        }
+    }
+}
