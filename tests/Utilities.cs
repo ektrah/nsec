3b3a6f0da9d38512b3731a7fc29772b93570b8dc
ektrah <klaus.hartke@gmail.com>
2017-02-09 14:08:57 +0100

    Update KAT for Blake2b

--- tests/Utilities.cs
+++ tests/Utilities.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
+using System.Reflection;
 using NSec.Cryptography.Formatting;
+using Xunit;
 
 namespace NSec.Tests
 {
@@ -16,6 +19,21 @@ namespace NSec.Tests
         {
             for (var i = 0; i < array.Length; i++)
                 array[i] = value;
+        }
+
+        public static TheoryData<string, string, string> LoadTheoryData3(Type type)
+        {
+            var assembly = type.GetTypeInfo().Assembly;
+
+            using (var stream = assembly.GetManifestResourceStream(type.FullName + ".txt"))
+            using (var reader = new StreamReader(stream))
+            {
+                var data = new TheoryData<string, string, string>();
+                var line = (string[])null;
+                while ((line = reader.ReadLine()?.Split(':')) != null)
+                    data.Add(line[0], line[1], line[2]);
+                return data;
+            }
         }
 
         public static byte[] Substring(this byte[] array, int offset, int length)
