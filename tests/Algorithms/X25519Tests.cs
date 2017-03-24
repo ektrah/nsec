7ebe65b5c0715755d95962bd83a690966181333c
ektrah <klaus.hartke@gmail.com>
2017-03-24 16:32:56 +0100

    Use known test vectors

--- tests/Algorithms/X25519Tests.cs
+++ tests/Algorithms/X25519Tests.cs
@@ -6,39 +6,43 @@ namespace NSec.Tests.Algorithms
 {
     public static class X25519Tests
     {
-        [Fact]
-        public static void BitMasked()
+        public static readonly TheoryData<string, string, string> Rfc7748TestVectors = Rfc.X25519Tests.Rfc7748TestVectors;
+
+        [Theory]
+        [MemberData(nameof(Rfc7748TestVectors))]
+        public static void BitMaskedAgree(string privateKey, string publicKey, string sharedSecret)
         {
             var a = new X25519();
             var kdf = new HkdfSha256();
 
-            using (var kA = new Key(a))
-            using (var kB = new Key(a))
+            var pk1 = publicKey.DecodeHex();
+            var pk2 = publicKey.DecodeHex();
+
+            pk1[pk1.Length - 1] &= 0x7F;
+            pk2[pk2.Length - 1] |= 0x80;
+
+            using (var k = Key.Import(a, privateKey.DecodeHex(), KeyBlobFormat.RawPrivateKey))
+            using (var sharedSecretExpected = SharedSecret.Import(sharedSecret.DecodeHex()))
+            using (var sharedSecretActual1 = a.Agree(k, PublicKey.Import(a, pk1, KeyBlobFormat.RawPublicKey)))
+            using (var sharedSecretActual2 = a.Agree(k, PublicKey.Import(a, pk2, KeyBlobFormat.RawPublicKey)))
             {
-                var pk1 = kB.Export(KeyBlobFormat.RawPublicKey);
-                var pk2 = kB.Export(KeyBlobFormat.RawPublicKey);
+                var expected = kdf.Extract(sharedSecretExpected, ReadOnlySpan<byte>.Empty);
+                var actual1 = kdf.Extract(sharedSecretActual1, ReadOnlySpan<byte>.Empty);
+                var actual2 = kdf.Extract(sharedSecretActual2, ReadOnlySpan<byte>.Empty);
 
-                pk1[pk1.Length - 1] &= 0x7F;
-                pk2[pk2.Length - 1] |= 0x80;
-
-                using (var s1 = a.Agree(kA, PublicKey.Import(a, pk1, KeyBlobFormat.RawPublicKey)))
-                using (var s2 = a.Agree(kA, PublicKey.Import(a, pk2, KeyBlobFormat.RawPublicKey)))
-                {
-                    var b1 = kdf.Extract(s1, ReadOnlySpan<byte>.Empty);
-                    var b2 = kdf.Extract(s2, ReadOnlySpan<byte>.Empty);
-
-                    Assert.Equal(b1, b2);
-                }
+                Assert.Equal(expected, actual1);
+                Assert.Equal(expected, actual2);
             }
         }
 
-        [Fact]
-        public static void BitMaskedEqual()
+        [Theory]
+        [MemberData(nameof(Rfc7748TestVectors))]
+        public static void BitMaskedEquals(string privateKey, string publicKey, string sharedSecret)
         {
             var a = new X25519();
 
-            var pk1 = Utilities.RandomBytes.Slice(0, a.PublicKeySize).ToArray();
-            var pk2 = Utilities.RandomBytes.Slice(0, a.PublicKeySize).ToArray();
+            var pk1 = publicKey.DecodeHex();
+            var pk2 = publicKey.DecodeHex();
 
             pk1[pk1.Length - 1] &= 0x7F;
             pk2[pk2.Length - 1] |= 0x80;
