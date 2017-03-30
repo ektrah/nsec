206806915a8382cbeae81fba737064be6ad038d3
ektrah <klaus.hartke@gmail.com>
2017-03-30 16:08:35 +0200

    Add Read/Write methods

--- src/Cryptography/Utilities.cs
+++ src/Cryptography/Utilities.cs
@@ -6,21 +6,74 @@ namespace NSec.Cryptography
     internal static class Utilities
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T Read<T>(
+            this ReadOnlySpan<byte> span)
+            where T : struct
+        {
+            if (span.Length < Unsafe.SizeOf<T>())
+                throw new ArgumentException();
+
+            return Unsafe.ReadUnaligned<T>(ref span.DangerousGetPinnableReference());
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint ReadBigEndian(
+            this ReadOnlySpan<byte> span)
+        {
+            return BitConverter.IsLittleEndian ? Reverse(span.Read<uint>()) : span.Read<uint>();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint ReadLittleEndian(
+            this ReadOnlySpan<byte> span)
+        {
+            return BitConverter.IsLittleEndian ? span.Read<uint>() : Reverse(span.Read<uint>());
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static uint ToBigEndian(
             uint value)
         {
-            if (BitConverter.IsLittleEndian)
-            {
-                return unchecked(
-                    (value << 24) |
-                    ((value & 0xFF00) << 8) |
-                    ((value & 0xFF0000) >> 8) |
-                    (value >> 24));
-            }
-            else
-            {
-                return value;
-            }
+            return BitConverter.IsLittleEndian ? Reverse(value) : value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write<T>(
+            this Span<byte> span,
+            T value)
+            where T : struct
+        {
+            if (span.Length < Unsafe.SizeOf<T>())
+                throw new ArgumentException();
+
+            Unsafe.WriteUnaligned(ref span.DangerousGetPinnableReference(), value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteBigEndian(
+            this Span<byte> span,
+            uint value)
+        {
+            span.Write(BitConverter.IsLittleEndian ? Reverse(value) : value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteLittleEndian(
+            this Span<byte> span,
+            uint value)
+        {
+            span.Write(BitConverter.IsLittleEndian ? value : Reverse(value));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint Reverse(
+            uint value)
+        {
+            return unchecked(
+                (value << 24) |
+                ((value & 0xFF00) << 8) |
+                ((value & 0xFF0000) >> 8) |
+                (value >> 24));
         }
     }
 }
