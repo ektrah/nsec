30e6f4ba8bc604c2fc994cb9455dfdb54749ecfe
ektrah <klaus.hartke@gmail.com>
2017-03-30 16:08:44 +0200

    Add RawKeyFormatter class

---
+++ src/Cryptography/Formatting/RawKeyFormatter.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Diagnostics;
+using static Interop.Libsodium;
+
+namespace NSec.Cryptography.Formatting
+{
+    internal class RawKeyFormatter
+    {
+        private readonly int _maxKeySize;
+        private readonly int _minKeySize;
+
+        public RawKeyFormatter(
+            int minKeySize,
+            int maxKeySize)
+        {
+            Debug.Assert(minKeySize >= 0);
+            Debug.Assert(maxKeySize >= minKeySize);
+
+            _minKeySize = minKeySize;
+            _maxKeySize = maxKeySize;
+        }
+
+        public byte[] Export(
+            SecureMemoryHandle keyHandle)
+        {
+            Debug.Assert(keyHandle != null);
+            Debug.Assert(keyHandle.Length >= _minKeySize);
+            Debug.Assert(keyHandle.Length <= _maxKeySize);
+
+            byte[] blob = new byte[keyHandle.Length];
+            keyHandle.Export(blob);
+            return blob;
+        }
+
+        public int GetBlobSize(
+            int keySize)
+        {
+            return keySize;
+        }
+
+        public bool TryImport(
+            ReadOnlySpan<byte> blob,
+            out SecureMemoryHandle keyHandle,
+            out byte[] publicKeyBytes)
+        {
+            int keySize = blob.Length;
+
+            if (keySize < _minKeySize || keySize > _maxKeySize)
+            {
+                keyHandle = null;
+                publicKeyBytes = null;
+                return false;
+            }
+
+            publicKeyBytes = null;
+            SecureMemoryHandle.Alloc(keySize, out keyHandle);
+            keyHandle.Import(blob);
+            return true;
+        }
+    }
+}
