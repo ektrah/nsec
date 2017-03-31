51551020161391ba0d5626c26daa5fa4c8c3465a
ektrah <klaus.hartke@gmail.com>
2017-03-31 13:11:52 +0200

    Rename magic value to blobHeader

--- src/Cryptography/Formatting/NSecKeyFormatter.cs
+++ src/Cryptography/Formatting/NSecKeyFormatter.cs
@@ -6,22 +6,22 @@ namespace NSec.Cryptography.Formatting
 {
     internal class NSecKeyFormatter
     {
-        private readonly byte[] _magic;
+        private readonly byte[] _blobHeader;
         private readonly int _maxKeySize;
         private readonly int _minKeySize;
 
         public NSecKeyFormatter(
             int minKeySize,
             int maxKeySize,
-            byte[] magic)
+            byte[] blobHeader)
         {
             Debug.Assert(minKeySize >= 0);
             Debug.Assert(maxKeySize >= minKeySize);
-            Debug.Assert(magic != null);
+            Debug.Assert(blobHeader != null);
 
             _minKeySize = minKeySize;
             _maxKeySize = maxKeySize;
-            _magic = magic;
+            _blobHeader = blobHeader;
         }
 
         public byte[] Export(
@@ -31,10 +31,10 @@ namespace NSec.Cryptography.Formatting
             Debug.Assert(keyHandle.Length >= _minKeySize);
             Debug.Assert(keyHandle.Length <= _maxKeySize);
 
-            byte[] blob = new byte[_magic.Length + sizeof(uint) + keyHandle.Length];
-            _magic.AsSpan().CopyTo(blob);
-            blob.AsSpan().Slice(_magic.Length).WriteLittleEndian((uint)keyHandle.Length);
-            keyHandle.Export(blob.AsSpan().Slice(_magic.Length + sizeof(uint)));
+            byte[] blob = new byte[_blobHeader.Length + sizeof(uint) + keyHandle.Length];
+            _blobHeader.AsSpan().CopyTo(blob);
+            blob.AsSpan().Slice(_blobHeader.Length).WriteLittleEndian((uint)keyHandle.Length);
+            keyHandle.Export(blob.AsSpan().Slice(_blobHeader.Length + sizeof(uint)));
             return blob;
         }
 
@@ -49,13 +49,13 @@ namespace NSec.Cryptography.Formatting
             out SecureMemoryHandle keyHandle,
             out byte[] publicKeyBytes)
         {
-            int keySize = blob.Length - (_magic.Length + sizeof(uint));
+            int keySize = blob.Length - (_blobHeader.Length + sizeof(uint));
 
             if (keySize < _minKeySize ||
                 keySize > _maxKeySize ||
-                blob.Length < _magic.Length + sizeof(uint) ||
-                !blob.StartsWith(_magic) ||
-                blob.Slice(_magic.Length).ReadLittleEndian() != (uint)keySize)
+                blob.Length < _blobHeader.Length + sizeof(uint) ||
+                !blob.StartsWith(_blobHeader) ||
+                blob.Slice(_blobHeader.Length).ReadLittleEndian() != (uint)keySize)
             {
                 keyHandle = null;
                 publicKeyBytes = null;
@@ -64,7 +64,7 @@ namespace NSec.Cryptography.Formatting
 
             publicKeyBytes = null;
             SecureMemoryHandle.Alloc(keySize, out keyHandle);
-            keyHandle.Import(blob.Slice(_magic.Length + sizeof(uint)));
+            keyHandle.Import(blob.Slice(_blobHeader.Length + sizeof(uint)));
             return true;
         }
     }
