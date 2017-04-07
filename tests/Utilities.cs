dd397b2e67d34819bfe9163a631f20d85e653c3c
ektrah <klaus.hartke@gmail.com>
2017-04-08 00:53:52 +0200

    Revert to old Base16 implementation

--- tests/Utilities.cs
+++ tests/Utilities.cs
@@ -12,7 +12,10 @@ namespace NSec.Tests
 
         public static byte[] DecodeHex(this string s)
         {
-            return Base16.Decode(s);
+            var result = new byte[s.Length / 2];
+            for (var i = 0; i < result.Length; i++)
+                result[i] = (byte)((ConvertHexDigit(s[2 * i + 0]) << 4) | ConvertHexDigit(s[2 * i + 1]));
+            return result;
         }
 
         public static void Fill(byte[] array, byte value)
@@ -101,6 +104,18 @@ namespace NSec.Tests
             var result = new byte[length];
             Buffer.BlockCopy(array, offset, result, 0, length);
             return result;
+        }
+
+        private static int ConvertHexDigit(char val)
+        {
+            if (val >= '0' && val <= '9')
+                return (val - '0');
+            else if (val >= 'a' && val <= 'f')
+                return ((val - 'a') + 10);
+            else if (val >= 'A' && val <= 'F')
+                return ((val - 'A') + 10);
+            else
+                throw new Exception();
         }
 
         #region Random Bytes
