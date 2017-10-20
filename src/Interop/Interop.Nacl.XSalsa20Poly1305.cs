f8e7b453a9579e69d98c410f0bbda06b4c67c764
ektrah <klaus.hartke@gmail.com>
2017-10-20 21:08:35 +0200

    Add interop code for xsalsa20poly1305

---
+++ src/Interop/Interop.Nacl.XSalsa20Poly1305.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    internal static partial class Libsodium
+    {
+        internal const int crypto_secretbox_xsalsa20poly1305_KEYBYTES = 32;
+        internal const int crypto_secretbox_xsalsa20poly1305_MACBYTES = 16;
+        internal const int crypto_secretbox_xsalsa20poly1305_NONCEBYTES = 24;
+
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_secretbox_easy(
+            ref byte c,
+            ref byte m,
+            ulong mlen,
+            ref byte n,
+            SecureMemoryHandle k);
+
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_secretbox_open_easy(
+            ref byte m,
+            ref byte c,
+            ulong clen,
+            ref byte n,
+            SecureMemoryHandle k);
+
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr crypto_secretbox_primitive();
+
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern UIntPtr crypto_secretbox_xsalsa20poly1305_keybytes();
+
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern UIntPtr crypto_secretbox_xsalsa20poly1305_macbytes();
+
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern UIntPtr crypto_secretbox_xsalsa20poly1305_noncebytes();
+    }
+}
