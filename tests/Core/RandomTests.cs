52105eef49ca6ba12d323d3feab8f3d07123bf76
ektrah <klaus.hartke@gmail.com>
2017-10-23 17:34:02 +0200

    Add tests for Generate(U)Int32 methods

--- tests/Core/RandomTests.cs
+++ tests/Core/RandomTests.cs
@@ -60,5 +60,214 @@ namespace NSec.Tests.Core
         }
 
         #endregion
+
+        #region GenerateInt32 #1
+
+        [Fact]
+        public static void GenerateInt32()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                var actual = RandomGenerator.Default.GenerateInt32();
+                Assert.InRange(actual, 0, int.MaxValue);
+            }
+        }
+
+        #endregion
+
+        #region GenerateInt32 #2
+
+        [Fact]
+        public static void GenerateInt32WithPositiveUpperBound()
+        {
+            const int upperExclusive = 198400021;
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var actual = RandomGenerator.Default.GenerateInt32(upperExclusive);
+                Assert.InRange(actual, 0, upperExclusive - 1);
+            }
+        }
+
+        [Fact]
+        public static void GenerateInt32WithNegativeUpperBound()
+        {
+            const int upperExclusive = -198400021;
+
+            Assert.Throws<ArgumentOutOfRangeException>("maxValue", () => RandomGenerator.Default.GenerateInt32(upperExclusive));
+        }
+
+        [Fact]
+        public static void GenerateInt32WithUpperBoundZero()
+        {
+            const int upperExclusive = 0;
+
+            var actual = RandomGenerator.Default.GenerateInt32(upperExclusive);
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public static void GenerateInt32WithUpperBoundOne()
+        {
+            const int upperExclusive = 1;
+
+            var actual = RandomGenerator.Default.GenerateInt32(upperExclusive);
+            Assert.Equal(0, actual);
+        }
+
+        #endregion
+
+        #region GenerateInt32 #3
+
+        [Fact]
+        public static void GenerateInt32WithPositiveLowerBound()
+        {
+            const int lowerInclusive = 198400021;
+            const int upperExclusive = 2147480009;
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var actual = RandomGenerator.Default.GenerateInt32(lowerInclusive, upperExclusive);
+                Assert.InRange(actual, lowerInclusive, upperExclusive - 1);
+            }
+        }
+
+        [Fact]
+        public static void GenerateInt32WithNegativeLowerBound()
+        {
+            const int lowerInclusive = -198400021;
+            const int upperExclusive = 2147480009;
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var actual = RandomGenerator.Default.GenerateInt32(lowerInclusive, upperExclusive);
+                Assert.InRange(actual, lowerInclusive, upperExclusive - 1);
+            }
+        }
+
+        [Fact]
+        public static void GenerateInt32WithInvalidLowerBound()
+        {
+            const int lowerInclusive = 2147480009;
+            const int upperExclusive = 198400021;
+
+            Assert.Throws<ArgumentException>("minValue", () => RandomGenerator.Default.GenerateInt32(lowerInclusive, upperExclusive));
+        }
+
+        [Fact]
+        public static void GenerateInt32WithSameLowerBoundZero()
+        {
+            const int lowerInclusive = 198400021;
+            const int upperExclusive = 198400021 + 0;
+
+            var actual = RandomGenerator.Default.GenerateInt32(lowerInclusive, upperExclusive);
+            Assert.Equal(lowerInclusive, actual);
+        }
+
+        [Fact]
+        public static void GenerateInt32WithSameLowerBoundOne()
+        {
+            const int lowerInclusive = 198400021;
+            const int upperExclusive = 198400021 + 1;
+
+            var actual = RandomGenerator.Default.GenerateInt32(lowerInclusive, upperExclusive);
+            Assert.Equal(lowerInclusive, actual);
+        }
+
+        #endregion
+
+        #region GenerateUInt32 #1
+
+        [Fact]
+        public static void GenerateUInt32()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                var actual = RandomGenerator.Default.GenerateUInt32();
+                Assert.InRange(actual, uint.MinValue, uint.MaxValue);
+            }
+        }
+
+        #endregion
+
+        #region GenerateUInt32 #2
+
+        [Fact]
+        public static void GenerateUInt32WithPositiveUpperBound()
+        {
+            const uint upperExclusive = 198400021;
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var actual = RandomGenerator.Default.GenerateUInt32(upperExclusive);
+                Assert.InRange(actual, uint.MinValue, upperExclusive - 1);
+            }
+        }
+
+        [Fact]
+        public static void GenerateUInt32WithUpperBoundZero()
+        {
+            const uint upperExclusive = 0;
+
+            var actual = RandomGenerator.Default.GenerateUInt32(upperExclusive);
+            Assert.Equal(uint.MinValue, actual);
+        }
+
+        [Fact]
+        public static void GenerateUInt32WithUpperBoundOne()
+        {
+            const uint upperExclusive = 1;
+
+            var actual = RandomGenerator.Default.GenerateUInt32(upperExclusive);
+            Assert.Equal(uint.MinValue, actual);
+        }
+
+        #endregion
+
+        #region GenerateUInt32 #3
+
+        [Fact]
+        public static void GenerateUInt32WithPositiveLowerBound()
+        {
+            const uint lowerInclusive = 198400021;
+            const uint upperExclusive = 2147480009;
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var actual = RandomGenerator.Default.GenerateUInt32(lowerInclusive, upperExclusive);
+                Assert.InRange(actual, lowerInclusive, upperExclusive - 1);
+            }
+        }
+
+        [Fact]
+        public static void GenerateUInt32WithInvalidLowerBound()
+        {
+            const uint lowerInclusive = 2147480009;
+            const uint upperExclusive = 198400021;
+
+            Assert.Throws<ArgumentException>("minValue", () => RandomGenerator.Default.GenerateUInt32(lowerInclusive, upperExclusive));
+        }
+
+        [Fact]
+        public static void GenerateUInt32WithSameLowerBoundZero()
+        {
+            const uint lowerInclusive = 198400021;
+            const uint upperExclusive = 198400021 + 0;
+
+            var actual = RandomGenerator.Default.GenerateUInt32(lowerInclusive, upperExclusive);
+            Assert.Equal(lowerInclusive, actual);
+        }
+
+        [Fact]
+        public static void GenerateUInt32WithSameLowerBoundOne()
+        {
+            const uint lowerInclusive = 198400021;
+            const uint upperExclusive = 198400021 + 1;
+
+            var actual = RandomGenerator.Default.GenerateUInt32(lowerInclusive, upperExclusive);
+            Assert.Equal(lowerInclusive, actual);
+        }
+
+        #endregion
     }
 }
