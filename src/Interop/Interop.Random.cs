2f5d2625e39d61d716d3152b6931a417dc90106b
ektrah <klaus.hartke@gmail.com>
2017-10-23 17:05:09 +0200

    Add randombytes_buf overload for uints

--- src/Interop/Interop.Random.cs
+++ src/Interop/Interop.Random.cs
@@ -9,5 +9,10 @@ internal static partial class Interop
         internal static extern void randombytes_buf(
             ref byte buf,
             UIntPtr size);
+
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void randombytes_buf(
+            out uint buf,
+            UIntPtr size);
     }
 }
