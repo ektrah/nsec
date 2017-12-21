4bace6b3c0eaeed106cb52710b78f92034a64edc
ektrah <klaus.hartke@gmail.com>
2017-12-21 12:58:02 +0100

    Set misuse handler

--- src/Cryptography/Sodium.cs
+++ src/Cryptography/Sodium.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using static Interop.Libsodium;
 
 namespace NSec.Cryptography
@@ -6,6 +7,7 @@ namespace NSec.Cryptography
     internal static class Sodium
     {
         private static readonly Lazy<bool> s_initialized = new Lazy<bool>(new Func<bool>(InitializeCore));
+        private static readonly MisuseHandler s_misuseHandler = new MisuseHandler(InternalError);
 
         public static void Initialize()
         {
@@ -26,9 +28,15 @@ namespace NSec.Cryptography
             // library had already been initialized. We call sodium_init() only
             // once, but if another library p/invokes into libsodium it might
             // have already been initialized.
-            return sodium_init() >= 0
-                && sodium_library_version_major() == SODIUM_LIBRARY_VERSION_MAJOR
-                && sodium_library_version_minor() == SODIUM_LIBRARY_VERSION_MINOR;
+            return sodium_library_version_major() == SODIUM_LIBRARY_VERSION_MAJOR
+                && sodium_library_version_minor() == SODIUM_LIBRARY_VERSION_MINOR
+                && sodium_set_misuse_handler(s_misuseHandler) == 0
+                && sodium_init() >= 0;
+        }
+
+        private static void InternalError()
+        {
+            throw Error.Cryptographic_InternalError();
         }
     }
 }
--- src/Interop/Interop.Core.cs
+++ src/Interop/Interop.Core.cs
@@ -5,7 +5,13 @@ internal static partial class Interop
 {
     internal static partial class Libsodium
     {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate void MisuseHandler();
+
         [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int sodium_init();
+
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int sodium_set_misuse_handler(MisuseHandler handler);
     }
 }
