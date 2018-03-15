3fa46a7d0ca2ba60715282084b3b079c39d75247
ektrah <klaus.hartke@gmail.com>
2018-03-15 10:25:49 +0100

    Nit

--- src/Interop/Interop.Core.cs
+++ src/Interop/Interop.Core.cs
@@ -10,6 +10,6 @@ internal static partial class Interop
 
         [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int sodium_set_misuse_handler(
-            System.Action handler);
+            Action handler);
     }
 }
