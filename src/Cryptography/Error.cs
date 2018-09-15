1d8c73490bf116bbbe2ea598e91205f3a89b49d7
ektrah <ektrah@users.noreply.github.com>
2018-09-15 16:45:56 +0200

    Consistency [ci skip]

--- src/Cryptography/Error.cs
+++ src/Cryptography/Error.cs
@@ -13,25 +13,25 @@ namespace NSec.Cryptography
         internal static ArgumentException Argument_BadBase16Length(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_BadBase16Length"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_BadBase16Length)), paramName);
         }
 
         internal static ArgumentException Argument_BadBase32Length(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_BadBase32Length"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_BadBase32Length)), paramName);
         }
 
         internal static ArgumentException Argument_BadBase64Length(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_BadBase64Length"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_BadBase64Length)), paramName);
         }
 
         internal static ArgumentException Argument_CiphertextLength(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_CiphertextLength"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_CiphertextLength)), paramName);
         }
 
         internal static ArgumentException Argument_DeriveInvalidCount(
@@ -44,7 +44,7 @@ namespace NSec.Cryptography
         internal static ArgumentException Argument_DestinationTooShort(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_DestinationTooShort"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_DestinationTooShort)), paramName);
         }
 
         internal static ArgumentException Argument_FormatNotSupported(
@@ -130,43 +130,43 @@ namespace NSec.Cryptography
         internal static ArgumentException Argument_NonceXorSize(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_NonceXorSize"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_NonceXorSize)), paramName);
         }
 
         internal static ArgumentException Argument_OverlapCiphertext(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_OverlapCiphertext"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_OverlapCiphertext)), paramName);
         }
 
         internal static ArgumentException Argument_OverlapInfo(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_OverlapInfo"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_OverlapInfo)), paramName);
         }
 
         internal static ArgumentException Argument_OverlapPlaintext(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_OverlapPlaintext"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_OverlapPlaintext)), paramName);
         }
 
         internal static ArgumentException Argument_OverlapPrk(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_OverlapPrk"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_OverlapPrk)), paramName);
         }
 
         internal static ArgumentException Argument_OverlapSalt(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_OverlapSalt"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_OverlapSalt)), paramName);
         }
 
         internal static ArgumentException Argument_PlaintextLength(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_PlaintextLength"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_PlaintextLength)), paramName);
         }
 
         internal static ArgumentException Argument_PlaintextTooLong(
@@ -186,7 +186,7 @@ namespace NSec.Cryptography
         internal static ArgumentException Argument_SaltNotSupported(
             string paramName)
         {
-            return new ArgumentException(ResourceManager.GetString("Argument_SaltNotSupported"), paramName);
+            return new ArgumentException(ResourceManager.GetString(nameof(Argument_SaltNotSupported)), paramName);
         }
 
         internal static ArgumentException Argument_SharedSecretLength(
@@ -206,31 +206,31 @@ namespace NSec.Cryptography
         internal static ArgumentNullException ArgumentNull_Algorithm(
             string paramName)
         {
-            return new ArgumentNullException(paramName, ResourceManager.GetString("ArgumentNull_Algorithm"));
+            return new ArgumentNullException(paramName, ResourceManager.GetString(nameof(ArgumentNull_Algorithm)));
         }
 
         internal static ArgumentNullException ArgumentNull_Key(
             string paramName)
         {
-            return new ArgumentNullException(paramName, ResourceManager.GetString("ArgumentNull_Key"));
+            return new ArgumentNullException(paramName, ResourceManager.GetString(nameof(ArgumentNull_Key)));
         }
 
         internal static ArgumentNullException ArgumentNull_Password(
             string paramName)
         {
-            return new ArgumentNullException(paramName, ResourceManager.GetString("ArgumentNull_Password"));
+            return new ArgumentNullException(paramName, ResourceManager.GetString(nameof(ArgumentNull_Password)));
         }
 
         internal static ArgumentNullException ArgumentNull_SharedSecret(
             string paramName)
         {
-            return new ArgumentNullException(paramName, ResourceManager.GetString("ArgumentNull_SharedSecret"));
+            return new ArgumentNullException(paramName, ResourceManager.GetString(nameof(ArgumentNull_SharedSecret)));
         }
 
         internal static ArgumentNullException ArgumentNull_String(
             string paramName)
         {
-            return new ArgumentNullException(paramName, ResourceManager.GetString("ArgumentNull_String"));
+            return new ArgumentNullException(paramName, ResourceManager.GetString(nameof(ArgumentNull_String)));
         }
 
         internal static ArgumentOutOfRangeException ArgumentOutOfRange_DeriveInvalidCount(
@@ -243,13 +243,13 @@ namespace NSec.Cryptography
         internal static ArgumentOutOfRangeException ArgumentOutOfRange_DeriveNegativeCount(
             string paramName)
         {
-            return new ArgumentOutOfRangeException(paramName, ResourceManager.GetString("ArgumentOutOfRange_DeriveNegativeCount"));
+            return new ArgumentOutOfRangeException(paramName, ResourceManager.GetString(nameof(ArgumentOutOfRange_DeriveNegativeCount)));
         }
 
         internal static ArgumentOutOfRangeException ArgumentOutOfRange_GenerateNegativeCount(
             string paramName)
         {
-            return new ArgumentOutOfRangeException(paramName, ResourceManager.GetString("ArgumentOutOfRange_GenerateNegativeCount"));
+            return new ArgumentOutOfRangeException(paramName, ResourceManager.GetString(nameof(ArgumentOutOfRange_GenerateNegativeCount)));
         }
 
         internal static ArgumentOutOfRangeException ArgumentOutOfRange_HashSize(
@@ -289,7 +289,7 @@ namespace NSec.Cryptography
         internal static ArgumentOutOfRangeException ArgumentOutOfRange_NonceAddend(
             string paramName)
         {
-            return new ArgumentOutOfRangeException(paramName, ResourceManager.GetString("ArgumentOutOfRange_NonceAddend"));
+            return new ArgumentOutOfRangeException(paramName, ResourceManager.GetString(nameof(ArgumentOutOfRange_NonceAddend)));
         }
 
         internal static ArgumentOutOfRangeException ArgumentOutOfRange_NonceCounterSize(
@@ -308,83 +308,83 @@ namespace NSec.Cryptography
 
         internal static FormatException Format_BadBase16()
         {
-            return new FormatException(ResourceManager.GetString("Format_BadBase16"));
+            return new FormatException(ResourceManager.GetString(nameof(Format_BadBase16)));
         }
 
         internal static FormatException Format_BadBase32()
         {
-            return new FormatException(ResourceManager.GetString("Format_BadBase32"));
+            return new FormatException(ResourceManager.GetString(nameof(Format_BadBase32)));
         }
 
         internal static FormatException Format_BadBase64()
         {
-            return new FormatException(ResourceManager.GetString("Format_BadBase64"));
+            return new FormatException(ResourceManager.GetString(nameof(Format_BadBase64)));
         }
 
         internal static FormatException Format_InvalidBlob()
         {
-            return new FormatException(ResourceManager.GetString("Format_InvalidBlob"));
+            return new FormatException(ResourceManager.GetString(nameof(Format_InvalidBlob)));
         }
 
         internal static InvalidOperationException InvalidOperation_AlreadyArchived()
         {
-            return new InvalidOperationException(ResourceManager.GetString("InvalidOperation_AlreadyArchived"));
+            return new InvalidOperationException(ResourceManager.GetString(nameof(InvalidOperation_AlreadyArchived)));
         }
 
         internal static InvalidOperationException InvalidOperation_ExportNotAllowed()
         {
-            return new InvalidOperationException(ResourceManager.GetString("InvalidOperation_ExportNotAllowed"));
+            return new InvalidOperationException(ResourceManager.GetString(nameof(InvalidOperation_ExportNotAllowed)));
         }
 
         internal static InvalidOperationException InvalidOperation_InitializationFailed()
         {
-            return new InvalidOperationException(ResourceManager.GetString("InvalidOperation_InitializationFailed"));
+            return new InvalidOperationException(ResourceManager.GetString(nameof(InvalidOperation_InitializationFailed)));
         }
 
         internal static InvalidOperationException InvalidOperation_InternalError()
         {
-            return new InvalidOperationException(ResourceManager.GetString("InvalidOperation_InternalError"));
+            return new InvalidOperationException(ResourceManager.GetString(nameof(InvalidOperation_InternalError)));
         }
 
         internal static InvalidOperationException InvalidOperation_UninitializedState()
         {
-            return new InvalidOperationException(ResourceManager.GetString("InvalidOperation_UninitializedState"));
+            return new InvalidOperationException(ResourceManager.GetString(nameof(InvalidOperation_UninitializedState)));
         }
 
         internal static NotSupportedException NotSupported_CreateKey()
         {
-            return new NotSupportedException(ResourceManager.GetString("NotSupported_CreateKey"));
+            return new NotSupportedException(ResourceManager.GetString(nameof(NotSupported_CreateKey)));
         }
 
         internal static NotSupportedException NotSupported_ExportKey()
         {
-            return new NotSupportedException(ResourceManager.GetString("NotSupported_ExportKey"));
+            return new NotSupportedException(ResourceManager.GetString(nameof(NotSupported_ExportKey)));
         }
 
         internal static NotSupportedException NotSupported_ImportKey()
         {
-            return new NotSupportedException(ResourceManager.GetString("NotSupported_ImportKey"));
+            return new NotSupportedException(ResourceManager.GetString(nameof(NotSupported_ImportKey)));
         }
 
         internal static NotSupportedException NotSupported_Operation()
         {
-            return new NotSupportedException(ResourceManager.GetString("NotSupported_Operation"));
+            return new NotSupportedException(ResourceManager.GetString(nameof(NotSupported_Operation)));
         }
 
         internal static OverflowException Overflow_NonceCounter()
         {
-            return new OverflowException(ResourceManager.GetString("Overflow_NonceCounter"));
+            return new OverflowException(ResourceManager.GetString(nameof(Overflow_NonceCounter)));
         }
 
         internal static PlatformNotSupportedException PlatformNotSupported_Algorithm()
         {
-            return new PlatformNotSupportedException(ResourceManager.GetString("PlatformNotSupported_Algorithm"));
+            return new PlatformNotSupportedException(ResourceManager.GetString(nameof(PlatformNotSupported_Algorithm)));
         }
 
         internal static PlatformNotSupportedException PlatformNotSupported_Initialization(
             Exception innerException)
         {
-            return new PlatformNotSupportedException(ResourceManager.GetString("PlatformNotSupported_Initialization"), innerException);
+            return new PlatformNotSupportedException(ResourceManager.GetString(nameof(PlatformNotSupported_Initialization)), innerException);
         }
     }
 }
