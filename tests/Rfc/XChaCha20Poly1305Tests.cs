7d8ad932cb5d27faa82d8836716fd792fce7c5f0
ektrah <ektrah@users.noreply.github.com>
2019-10-09 13:37:23 +0200

    Update I-D.irtf-cfrg-xchacha reference [ci skip]

--- tests/Rfc/XChaCha20Poly1305Tests.cs
+++ tests/Rfc/XChaCha20Poly1305Tests.cs
@@ -7,7 +7,7 @@ namespace NSec.Tests.Rfc
 {
     public static class XChaCha20Poly1305Tests
     {
-        // draft-irtf-cfrg-xchacha-00
+        // draft-irtf-cfrg-xchacha-01
         public static readonly TheoryData<string[]> TestVectors = new TheoryData<string[]>
         {
             // Appendix A.1
