498fda2626a6d41a5e45d8bce649b20fa3b9b9e9
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2020-02-14 17:52:42 +0100

    add ed25519 to x25519

---
+++ src/Interop/Interop.Ed25519ToX25519.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    internal static partial class Libsodium
+    {
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static unsafe extern int crypto_sign_ed25519_sk_to_curve25519(
+            byte* x25519_sk,
+            byte* ed25519_sk);
+
+        [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
+        internal static unsafe extern int crypto_sign_ed25519_pk_to_curve25519(
+            byte* x25519_pk,
+            byte* ed25519_pk);
+    }
+}
