0a4fb71cdc593ec45016ab50fe16382132a67e17
sigaloid <69441971+sigaloid@users.noreply.github.com>
2020-12-05 17:25:00 -0500

    Update Ed25519ToX25519.cs
    Needs some sort of error checking (also update tests)

--- src/Experimental/Ed25519ToX25519.cs
+++ src/Experimental/Ed25519ToX25519.cs
@@ -5,31 +5,31 @@ namespace NSec.Cryptography
 {
     public static class Ed25519ToX25519
     {
-        public unsafe static byte[] SignEd25519SecretToCurve25519(byte[] privateKey)
+        public unsafe static Key SignEd25519SecretToCurve25519(Key privateKey)
         {
             var xPrivateKey = new byte[32];
 
             fixed (byte* x25519_sk = xPrivateKey)
-            fixed (byte* ed25519_sk = privateKey)
+            fixed (byte* ed25519_sk = privateKey.Export(KeyBlobFormat.RawPrivateKey))
             {
                 int error = crypto_sign_ed25519_sk_to_curve25519(x25519_sk, ed25519_sk);
                 Debug.Assert(error == 0);
             }
 
-            return xPrivateKey;
+            return Key.Import(KeyAgreementAlgorithm.X25519, xPrivateKey, KeyBlobFormat.RawPrivateKey, new KeyCreationParameters { ExportPolicy = KeyExportPolicies.AllowPlaintextExport});
         }
-        public unsafe static byte[] SignEd25519PublicToCurve25519(byte[] publicKey)
+        public unsafe static PublicKey SignEd25519PublicToCurve25519(PublicKey publicKey)
         {
             var xPublicKey = new byte[32];
 
             fixed (byte* x25519_sk = xPublicKey)
-            fixed (byte* ed25519_sk = publicKey)
+            fixed (byte* ed25519_sk = publicKey.Export(KeyBlobFormat.RawPublicKey))
             {
                 int error = crypto_sign_ed25519_pk_to_curve25519(x25519_sk, ed25519_sk);
                 Debug.Assert(error == 0);
             }
 
-            return xPublicKey;
+            return PublicKey.Import(KeyAgreementAlgorithm.X25519, xPublicKey, KeyBlobFormat.RawPublicKey);
         }
     }
 }
