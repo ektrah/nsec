b0f2705e561c9a5e7e06521688b3761fe7a93433
ektrah <ektrah@users.noreply.github.com>
2020-12-27 14:03:26 +0100

    Add Ed25519ToX25519 functions to Interop.yaml

--- src/Interop/Interop.Ed25519ToX25519.cs
+++ src/Interop/Interop.Ed25519ToX25519.cs
@@ -6,13 +6,13 @@ internal static partial class Interop
     internal static partial class Libsodium
     {
         [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern int crypto_sign_ed25519_sk_to_curve25519(
-            byte* x25519_sk,
-            byte* ed25519_sk);
+        internal static unsafe extern int crypto_sign_ed25519_pk_to_curve25519(
+            byte* curve25519_pk,
+            byte* ed25519_pk);
 
         [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern int crypto_sign_ed25519_pk_to_curve25519(
-            byte* x25519_pk,
-            byte* ed25519_pk);
+        internal static unsafe extern int crypto_sign_ed25519_sk_to_curve25519(
+            byte* curve25519_sk,
+            byte* ed25519_sk);
     }
 }
