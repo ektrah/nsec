b2ef9c21ac1384d49474c4cb775dd0c4446cfc97
Daniel Nachtrub <dn@nuvotex.de>
2021-09-01 15:27:19 +0200

    added export to shared secret

--- src/Cryptography/SharedSecret.cs
+++ src/Cryptography/SharedSecret.cs
@@ -5,21 +5,39 @@ using static Interop.Libsodium;
 
 namespace NSec.Cryptography
 {
+    /// <summary>
+    /// represents a shared secret
+    /// </summary>
     [DebuggerDisplay("Size = {Size}")]
     public sealed class SharedSecret : IDisposable
     {
+        /// <summary>
+        /// handle to the underlying secure memory structure
+        /// </summary>
         private readonly SecureMemoryHandle _handle;
-
+        /// <summary>
+        /// ctor using a provided handle
+        /// </summary>
+        /// <param name="sharedSecretHandle"></param>
         internal SharedSecret(
             SecureMemoryHandle sharedSecretHandle)
         {
             _handle = sharedSecretHandle;
         }
-
+        /// <summary>
+        /// length of the key
+        /// </summary>
         public int Size => _handle.Size;
-
+        /// <summary>
+        /// internally exposed handle
+        /// </summary>
         internal SecureMemoryHandle Handle => _handle;
-
+        /// <summary>
+        /// imports a shared key
+        /// </summary>
+        /// <param name="sharedSecret"></param>
+        /// <param name="creationParameters"></param>
+        /// <returns></returns>
         [Obsolete("The 'Import' method is obsolete. Use the method overloads in the 'KeyDerivationAlgorithm' class that accept a 'ReadOnlySpan<byte>' directly instead.")]
         public static SharedSecret Import(
             ReadOnlySpan<byte> sharedSecret,
@@ -55,18 +73,35 @@ namespace NSec.Cryptography
 
             return new SharedSecret(sharedSecretHandle);
         }
-
+        /// <summary>
+        /// disposes the secret
+        /// </summary>
         public void Dispose()
         {
             _handle.Dispose();
         }
-
+        /// <summary>
+        /// to string
+        /// </summary>
+        /// <returns></returns>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override string? ToString()
         {
             return typeof(SharedSecret).ToString();
         }
-
+        /// <summary>
+        /// copies secret data to the given destination
+        /// </summary>
+        /// <param name="destination"></param>
+        public void CopyTo(Span<byte> destination)
+        {
+            _handle.CopyTo(destination);
+        }
+        /// <summary>
+        /// creates a new secret via import
+        /// </summary>
+        /// <param name="sharedSecret"></param>
+        /// <param name="sharedSecretHandle"></param>
         private static void ImportCore(
             ReadOnlySpan<byte> sharedSecret,
             out SecureMemoryHandle? sharedSecretHandle)
