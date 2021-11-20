55d81bb90e0d3d049a60da09dad06da877445ba9
ektrah <ektrah@users.noreply.github.com>
2021-11-20 16:03:44 +0100

    Add Hex/Base64 functions

--- src/Experimental/CryptographicUtilities.cs
+++ src/Experimental/CryptographicUtilities.cs
@@ -6,6 +6,18 @@ namespace NSec.Experimental
     public static class CryptographicUtilities
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte[] Base64Decode(string base64)
+        {
+            return NSec.Experimental.Text.Base64.Decode(base64);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Base64Encode(ReadOnlySpan<byte> bytes)
+        {
+            return NSec.Experimental.Text.Base64.Encode(bytes);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void FillRandomBytes(Span<byte> data)
         {
             System.Security.Cryptography.RandomNumberGenerator.Fill(data);
@@ -15,6 +27,18 @@ namespace NSec.Experimental
         public static bool FixedTimeEquals(ReadOnlySpan<byte> left, ReadOnlySpan<byte> right)
         {
             return System.Security.Cryptography.CryptographicOperations.FixedTimeEquals(left, right);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte[] HexDecode(string base16)
+        {
+            return NSec.Experimental.Text.Base16.Decode(base16);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string HexEncode(ReadOnlySpan<byte> bytes)
+        {
+            return NSec.Experimental.Text.Base16.Encode(bytes);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
