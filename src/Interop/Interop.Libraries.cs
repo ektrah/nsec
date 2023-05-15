e1431c6e6a98c986ce642bb80de4d323c179e811
ektrah <ektrah@users.noreply.github.com>
2023-05-15 16:55:19 +0200

    Add initial support for iOS

--- src/Interop/Interop.Libraries.cs
+++ src/Interop/Interop.Libraries.cs
@@ -2,6 +2,10 @@ internal static partial class Interop
 {
     private static class Libraries
     {
+#if IOS
+        internal const string Libsodium = "__Internal";
+#else
         internal const string Libsodium = "libsodium";
+#endif
     }
 }
