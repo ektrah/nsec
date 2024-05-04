4a3bc4b1dd4e2986600054a2925c6e3ee8dcf45c
ektrah <ektrah@users.noreply.github.com>
2024-05-04 16:35:11 +0200

    Add support for iOS, tvOS, and MacCatalyst

--- src/Interop/Interop.Libraries.cs
+++ src/Interop/Interop.Libraries.cs
@@ -2,6 +2,10 @@ internal static partial class Interop
 {
     private static class Libraries
     {
+#if IOS || TVOS || MACCATALYST
+        internal const string Libsodium = "__Internal";
+#else
         internal const string Libsodium = "libsodium";
+#endif
     }
 }
